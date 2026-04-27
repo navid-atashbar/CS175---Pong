fbc905fa1cbe9c139413aa20dd97fe53d9726993
Navid Atashbar <atashbar@uci.edu>
2026-04-26 22:56:59 -0700

    finished random agent to randomly pick a move throughout the game (move up down or stay still)

--- random_agent.py
+++ random_agent.py
@@ -1,1 +1,43 @@
 
+import ale_py
+import gymnasium as gym
+import pygame
+import random
+gym.register_envs(ale_py)
+
+env = gym.make("ALE/Pong-v5", render_mode="human")
+obs, info = env.reset()
+
+clock = pygame.time.Clock()
+done = False
+score = 0
+
+print("Use UP and DOWN arrow keys to play!")
+print("Close the window to quit.")
+
+while not done:
+    action = 0  # default: stay still
+    keys = random.choice([0,2,3])
+    
+    if keys == 2:
+        action = 2  # move up
+    elif keys ==3:
+        action = 3  # move down
+
+    # handle quit
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    obs, reward, terminated, truncated, info = env.step(action)
+
+    if reward == 1:
+        print("You scored!")
+    elif reward == -1:
+        print("Opponent scored!")
+
+    done = terminated or truncated
+    clock.tick(30)  # 30 FPS
+
+print("Game over!")
+env.close()
